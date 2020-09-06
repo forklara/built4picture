789511efb70de706d41999bfb82040b32da03b98
root <root@preonline-lizhihao-12.dev.kwaidc.com>
2020-09-06 10:18:04 +0800

    new file

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int first, int second)
+{
+	return first + second;
+}
+
+int main()
+{
+	std::cout << sum(1, 2) << std::endl;
+	return 0;
+}
+
